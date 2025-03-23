0fd1963af62af009b7a379174461b823d1fdcd92
Even Bell <130771607+2000e@users.noreply.github.com>
2025-03-23 20:24:20 +0800

    Update
    - 更新Set-Proxy 指令默认端口7897 (Clash Verge默认)
    - 更新Windows终端打开时未指定初始位置过滤系统文件夹和MyFindDocker路径
    - 修改 where.exe等几个引用，添加几个常用的引用

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,7 @@ Invoke-Expression (& { (zoxide.exe init powershell | Out-String) })
 function Set-Proxy {
     param (
         [string]$HostIp = "127.0.0.1",
-        [int]$Port = 7890
+        [int]$Port = 7897 # Clash Verge
     )
     $env:http_proxy = "http://${HostIp}:$Port"
     $env:https_proxy = "http://${HostIp}:$Port"
@@ -18,9 +18,8 @@ function Remove-Proxy {
 }
 
 # Some useful alias
-Remove-Alias cd -Scope Local
 Set-Alias open  explorer.exe
-Set-Alias check where.exe
+Set-Alias which where.exe
 Set-Alias edit  notepad.exe
 Set-Alias new   ni
 Set-Alias n     ni
@@ -28,12 +27,14 @@ Set-Alias touch ni
 Set-Alias grep  findstr
 
 # app
+Remove-Alias cd -Scope Local    #This for zoxide
 Set-Alias cd    __zoxide_z
 Set-Alias s     sudo
-Set-Alias v     nvim.exe    # modify this if you preffer vim
+Set-Alias vim   nvim.exe    # modify this if you preffer vim
 Set-Alias .net  dotnet.exe
 Set-Alias dk    docker.exe
 Set-Alias pn    pnpm
+Set-Alias py    python
 
 # cd to home dir when launch powsershell by some apps without Specified Location.
-if ( (Get-Location).Path -eq "C:\Windows\System32" ) { Set-Location $HOME }
+if ( (Get-Location).Path -match "\Windows\System32|MyDockFinder" ) { Set-Location $HOME }
